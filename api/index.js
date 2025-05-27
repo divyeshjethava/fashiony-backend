32a8e659af78a99c25e159ca595c9c4a53ba89dd
divyeshjethava <jethavadivyesh679@gmail.com>
2025-05-27 23:47:50 +0530

    solving the import problem

--- api/index.js
+++ api/index.js
@@ -1,39 +1,33 @@
 import express from 'express';
+import serverless from 'serverless-http';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import serverless from 'serverless-http'; 
-
 import userRoutes from '../routes/userRoutes.js'; 
 
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
-
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('MongoDB connection error:', error.message);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err);
     process.exit(1);
   }
 };
-connectDB();
 
+connectDB();
 
 app.use('/api', userRoutes);
 
-
-app.use((req, res, next) => {
-  console.log(`Incoming request: ${req.method} ${req.url}`);
-  next();
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
 });
-
 
 export const handler = serverless(app);
